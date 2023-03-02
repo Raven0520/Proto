355f5bd5445a610b5ca1cd7081611fd8c272bb9c
Raven <raven.259695@gmail.com>
2023-03-02 15:49:35 +0800

    Initialization BTC Proto

--- btc/global.pb.go
+++ btc/global.pb.go
@@ -4,9 +4,9 @@
 // versions:
 // 	protoc-gen-go v1.26.0
 // 	protoc        v3.19.1
-// source: global.proto
+// source: btc/global.proto
 
-package pb
+package btc
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
@@ -31,7 +31,7 @@ type EmptyPost struct {
 func (x *EmptyPost) Reset() {
 	*x = EmptyPost{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_global_proto_msgTypes[0]
+		mi := &file_btc_global_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -44,7 +44,7 @@ func (x *EmptyPost) String() string {
 func (*EmptyPost) ProtoMessage() {}
 
 func (x *EmptyPost) ProtoReflect() protoreflect.Message {
-	mi := &file_global_proto_msgTypes[0]
+	mi := &file_btc_global_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -57,7 +57,7 @@ func (x *EmptyPost) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use EmptyPost.ProtoReflect.Descriptor instead.
 func (*EmptyPost) Descriptor() ([]byte, []int) {
-	return file_global_proto_rawDescGZIP(), []int{0}
+	return file_btc_global_proto_rawDescGZIP(), []int{0}
 }
 
 // StringResponse Return String
@@ -73,7 +73,7 @@ type StringResponse struct {
 func (x *StringResponse) Reset() {
 	*x = StringResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_global_proto_msgTypes[1]
+		mi := &file_btc_global_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -86,7 +86,7 @@ func (x *StringResponse) String() string {
 func (*StringResponse) ProtoMessage() {}
 
 func (x *StringResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_global_proto_msgTypes[1]
+	mi := &file_btc_global_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -99,7 +99,7 @@ func (x *StringResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use StringResponse.ProtoReflect.Descriptor instead.
 func (*StringResponse) Descriptor() ([]byte, []int) {
-	return file_global_proto_rawDescGZIP(), []int{1}
+	return file_btc_global_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *StringResponse) GetMessage() string {
@@ -116,37 +116,38 @@ func (x *StringResponse) GetData() string {
 	return ""
 }
 
-var File_global_proto protoreflect.FileDescriptor
+var File_btc_global_proto protoreflect.FileDescriptor
 
-var file_global_proto_rawDesc = []byte{
-	0x0a, 0x0c, 0x67, 0x6c, 0x6f, 0x62, 0x61, 0x6c, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x02,
-	0x70, 0x62, 0x22, 0x0b, 0x0a, 0x09, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x50, 0x6f, 0x73, 0x74, 0x22,
-	0x3e, 0x0a, 0x0e, 0x53, 0x74, 0x72, 0x69, 0x6e, 0x67, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
-	0x65, 0x12, 0x18, 0x0a, 0x07, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01,
-	0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x44,
-	0x61, 0x74, 0x61, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x42,
-	0x13, 0x42, 0x06, 0x67, 0x6c, 0x6f, 0x62, 0x61, 0x6c, 0x50, 0x01, 0x5a, 0x07, 0x2e, 0x2f, 0x70,
-	0x62, 0x3b, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_btc_global_proto_rawDesc = []byte{
+	0x0a, 0x10, 0x62, 0x74, 0x63, 0x2f, 0x67, 0x6c, 0x6f, 0x62, 0x61, 0x6c, 0x2e, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x12, 0x03, 0x62, 0x74, 0x63, 0x22, 0x0b, 0x0a, 0x09, 0x45, 0x6d, 0x70, 0x74, 0x79,
+	0x50, 0x6f, 0x73, 0x74, 0x22, 0x3e, 0x0a, 0x0e, 0x53, 0x74, 0x72, 0x69, 0x6e, 0x67, 0x52, 0x65,
+	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67,
+	0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65,
+	0x12, 0x12, 0x0a, 0x04, 0x44, 0x61, 0x74, 0x61, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04,
+	0x64, 0x61, 0x74, 0x61, 0x42, 0x15, 0x42, 0x06, 0x67, 0x6c, 0x6f, 0x62, 0x61, 0x6c, 0x50, 0x01,
+	0x5a, 0x09, 0x2e, 0x2f, 0x62, 0x74, 0x63, 0x3b, 0x62, 0x74, 0x63, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 }
 
 var (
-	file_global_proto_rawDescOnce sync.Once
-	file_global_proto_rawDescData = file_global_proto_rawDesc
+	file_btc_global_proto_rawDescOnce sync.Once
+	file_btc_global_proto_rawDescData = file_btc_global_proto_rawDesc
 )
 
-func file_global_proto_rawDescGZIP() []byte {
-	file_global_proto_rawDescOnce.Do(func() {
-		file_global_proto_rawDescData = protoimpl.X.CompressGZIP(file_global_proto_rawDescData)
+func file_btc_global_proto_rawDescGZIP() []byte {
+	file_btc_global_proto_rawDescOnce.Do(func() {
+		file_btc_global_proto_rawDescData = protoimpl.X.CompressGZIP(file_btc_global_proto_rawDescData)
 	})
-	return file_global_proto_rawDescData
+	return file_btc_global_proto_rawDescData
 }
 
-var file_global_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_global_proto_goTypes = []interface{}{
-	(*EmptyPost)(nil),      // 0: pb.EmptyPost
-	(*StringResponse)(nil), // 1: pb.StringResponse
+var file_btc_global_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_btc_global_proto_goTypes = []interface{}{
+	(*EmptyPost)(nil),      // 0: btc.EmptyPost
+	(*StringResponse)(nil), // 1: btc.StringResponse
 }
-var file_global_proto_depIdxs = []int32{
+var file_btc_global_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -154,13 +155,13 @@ var file_global_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_global_proto_init() }
-func file_global_proto_init() {
-	if File_global_proto != nil {
+func init() { file_btc_global_proto_init() }
+func file_btc_global_proto_init() {
+	if File_btc_global_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_global_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_btc_global_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*EmptyPost); i {
 			case 0:
 				return &v.state
@@ -172,7 +173,7 @@ func file_global_proto_init() {
 				return nil
 			}
 		}
-		file_global_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_btc_global_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*StringResponse); i {
 			case 0:
 				return &v.state
@@ -189,18 +190,18 @@ func file_global_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_global_proto_rawDesc,
+			RawDescriptor: file_btc_global_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_global_proto_goTypes,
-		DependencyIndexes: file_global_proto_depIdxs,
-		MessageInfos:      file_global_proto_msgTypes,
+		GoTypes:           file_btc_global_proto_goTypes,
+		DependencyIndexes: file_btc_global_proto_depIdxs,
+		MessageInfos:      file_btc_global_proto_msgTypes,
 	}.Build()
-	File_global_proto = out.File
-	file_global_proto_rawDesc = nil
-	file_global_proto_goTypes = nil
-	file_global_proto_depIdxs = nil
+	File_btc_global_proto = out.File
+	file_btc_global_proto_rawDesc = nil
+	file_btc_global_proto_goTypes = nil
+	file_btc_global_proto_depIdxs = nil
 }
